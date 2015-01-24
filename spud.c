f309c130d912d27d3090da587f4a1ba93b382ee5
Stephen Haywood <stephen@averagesecurityguy.info>
2015-01-23 22:38:51 -0500

    Update comments fix line wraps.

--- spud.c
+++ spud.c
@@ -1,3 +1,7 @@
+/*
+* A simple program to encrypt and decrypt files with a passphrase using
+* libsodium, which is available here, http://doc.libsodium.org/index.html.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,9 +16,10 @@
 
 
 /*
-Function: open_file
--------------------
-Attempt to open a file. If there is an error write it to stderr and return NULL.
+* Function: open_file
+* -------------------
+* Attempt to open a file. If there is an error write it to stderr and return
+* NULL.
 */
 FILE *open_file(char* fname, char *mode)
 {
@@ -30,18 +35,20 @@ FILE *open_file(char* fname, char *mode)
 
 
 /*
-Function: generate_key_nonce
-----------------------------
-Attempt to generate a key and nonce using the passphrase and the static salt
-values. If there are any errors then return -1 otherwise return 0.
+* Function: generate_key_nonce
+* ----------------------------
+* Attempt to generate a key and nonce using the passphrase and the static salt
+* values. If there are any errors then return -1 otherwise return 0.
 */
-int generate_key_nonce(unsigned char *key, unsigned char *nonce, char *passphrase)
+int generate_key_nonce(unsigned char *key, unsigned char *nonce,
+                       char *passphrase)
 {
     char *key_salt = "wJDNGf7/Jrce41GTllX+Z4I0eHdva+IXFsYiD5Sg50M";
     char *nonce_salt = "6K9qNULNb0M61brPbbDFrA7zp8DULGb5G5tVRRpRFqk";
 
     if (crypto_pwhash_scryptsalsa208sha256
-        (key, KEYBYTES, passphrase, strlen(passphrase), (unsigned char *)key_salt,
+        (key, KEYBYTES, passphrase, strlen(passphrase),
+        (unsigned char *)key_salt,
         crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE,
         crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_INTERACTIVE) != 0)
     {
@@ -50,7 +57,8 @@ int generate_key_nonce(unsigned char *key, unsigned char *nonce, char *passphras
     }
 
     if (crypto_pwhash_scryptsalsa208sha256
-        (nonce, NONCEBYTES, passphrase, strlen(passphrase), (unsigned char *)nonce_salt,
+        (nonce, NONCEBYTES, passphrase, strlen(passphrase),
+        (unsigned char *)nonce_salt,
         crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE,
         crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_INTERACTIVE) != 0)
     {
@@ -63,12 +71,13 @@ int generate_key_nonce(unsigned char *key, unsigned char *nonce, char *passphras
 
 
 /*
-Function: encrypt_file
-----------------------
-Encrypt a file by reading in chunks of data, encrypting those chunks, and
-writing the encrypted chunks to a new file.
+* Function: encrypt_file
+* ----------------------
+* Encrypt a file by reading in chunks of data, encrypting those chunks, and
+* writing the encrypted chunks to a new file.
 */
-int encrypt_file(char *pfname, char *efname, unsigned char *k, unsigned char *n)
+int encrypt_file(char *pfname, char *efname, unsigned char *k,
+                 unsigned char *n)
 {
     size_t rsize;
     unsigned char ebuf[BUFBYTES + MACBYTES];
@@ -110,12 +119,13 @@ int encrypt_file(char *pfname, char *efname, unsigned char *k, unsigned char *n)
 
 
 /*
-Function: decrypt_file
-----------------------
-Decrypt a file by reading in chunks of data, decrypting those chunks, and
-writing the decrypted chunks to a new file.
+* Function: decrypt_file
+* ----------------------
+* Decrypt a file by reading in chunks of data, decrypting those chunks, and
+* writing the decrypted chunks to a new file.
 */
-int decrypt_file(char *efname, char *pfname, unsigned char *k, unsigned char *n)
+int decrypt_file(char *efname, char *pfname, unsigned char *k,
+                 unsigned char *n)
 {
     size_t rsize;
     unsigned char ebuf[BUFBYTES + MACBYTES];
@@ -154,9 +164,9 @@ int decrypt_file(char *efname, char *pfname, unsigned char *k, unsigned char *n)
 
 
 /*
-Function: usage
----------------
-Write out the usage statement and exit the program.
+* Function: usage
+* ---------------
+* Write out the usage statement and exit the program.
 */
 void usage()
 {
@@ -167,10 +177,10 @@ void usage()
 
 
 /*
-Function: get_passphrase
-------------------------
-Prompt the user for their passphrase. Store it in the buffer represented by
-passphrase. If there is an error, exit the program.
+* Function: get_passphrase
+* ------------------------
+* Prompt the user for their passphrase. Store it in the buffer represented by
+* passphrase. If there is an error, exit the program.
 */
 void get_passphrase(char *p)
 {
@@ -186,12 +196,13 @@ void get_passphrase(char *p)
 
 
 /*
-Function: get_key_material
---------------------------
-Attempt to get the needed key material to encrypt or decrypt the file. If
-there is an error, exit the program.
+* Function: get_key_material
+* --------------------------
+* Attempt to get the needed key material to encrypt or decrypt the file. If
+* there is an error, exit the program.
 */
-void get_key_material(unsigned char *key, unsigned char *nonce, char *passphrase)
+void get_key_material(unsigned char *key, unsigned char *nonce,
+                      char *passphrase)
 {
     if (generate_key_nonce(key, nonce, passphrase) == -1)
     {
@@ -202,12 +213,13 @@ void get_key_material(unsigned char *key, unsigned char *nonce, char *passphrase
 
 
 /*
-Function: process_file
-----------------------
-Attempt to either encrypt or decrypt the infile using the key and nonce
-provided. If there is an error, exit the program.
+* Function: process_file
+* ----------------------
+* Attempt to either encrypt or decrypt the infile using the key and nonce
+* provided. If there is an error, exit the program.
 */
-void process_file(char *command, char *infile, char *outfile, unsigned char *key, unsigned char *nonce)
+void process_file(char *command, char *infile, char *outfile,
+                  unsigned char *key, unsigned char *nonce)
 {
     int result = 0;
 
@@ -231,9 +243,9 @@ void process_file(char *command, char *infile, char *outfile, unsigned char *key
 
 
 /*
-Function: main
---------------
-Where it all begins.
+* Function: main
+* --------------
+* Where it all begins.
 */
 int main(int argc, char *argv[])
 {
