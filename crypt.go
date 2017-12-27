f6fe108638f035dfae01589fb9aae867e7e4460d
Stephen Haywood <stephen@averagesecurityguy.info>
2017-12-26 22:54:52 -0500

    Update

--- crypt.go
+++ crypt.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-    "bufio"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
 	"fmt"
-	"io"
+    "io/ioutil"
 	"os"
-    "strings"
     "syscall"
 
     "golang.org/x/crypto/ssh/terminal"
@@ -17,7 +14,7 @@ import (
 
 
 type Cryptor struct {
-	Aead *cipher.AEAD
+	Aead cipher.AEAD
 	Nonce []byte
 }
 
@@ -35,19 +32,21 @@ func NewCryptor(passphrase string) (*Cryptor, error) {
 	// Static salt used to seed Argon2. Generated with:
 	// head -c 32 /dev/urandom | base64
 	salt := []byte("AKatmtgdkMKq5SFYLt8tBlUxuwLccdCjFfFNi2b3o9A")
-	kdf := argon2.Key([]byte(passphrase), salt, 4, 32*1024, 4, 56)
+	kdf := argon2.Key([]byte(passphrase), salt, 4, 32*1024, 4, 44)
 
 	c.Nonce = kdf[32:]
 
 	block, err := aes.NewCipher(kdf[:32])
 	if err != nil {
-		c, err
+		return c, err
 	}
 
-	c.Aead, err := cipher.NewGCM(block)
+	aead, err := cipher.NewGCM(block)
 	if err != nil {
-		c, err
+		return c, err
 	}
+
+    c.Aead = aead
 
 	return c, nil
 }
@@ -57,22 +56,27 @@ func getPass(prompt string) string {
     fmt.Print(prompt)
     pass, err := terminal.ReadPassword(int(syscall.Stdin))
     if err != nil {
-		os.Exit("Could not read password.")
+        fmt.Println("\nCould not read password.")
+		os.Exit(0)
 	}
 
-	pass = strings.TrimSpace(pass)
+    fmt.Println("")
 
-	if len(pass) < 16 {
-		os.Exit("Password must be at least 16 characters.")
-	}
-
-	return pass
+	return string(pass)
 }
 
 func save(name string, data []byte) {
 	err := ioutil.WriteFile(name, data, 0644)
 	check(err)
 }
+
+func check(e error) {
+    if e != nil {
+        fmt.Printf("Error: %s\n", e.Error())
+        os.Exit(0)
+    }
+}
+
 
 func usage() {
 	fmt.Println("Usage: crypt command infile outfile")
@@ -82,29 +86,39 @@ func usage() {
 
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) != 4 {
 		usage()
 	}
 
 	// Parse arguments
-	action := os.Args[0]
-	iFile := os.Args[1]
-	oFile := os.Args[2]
+	action := os.Args[1]
+	iFile := os.Args[2]
+	oFile := os.Args[3]
 
 	// Get Password
-	pass1 = getPass("Enter password: ")
-	pass2 = getPass("Enter password again: ")
+	pass1 := getPass("Enter password: ")
+	pass2 := getPass("Enter password again: ")
 
 	// Compare passwords.
 	if pass1 != pass2 {
-		os.Exit("Passwords do not match.")
+		fmt.Println("Passwords do not match.")
+        os.Exit(0)
 	}
+
+    // Check password length
+    if len(pass1) < 16 {
+		fmt.Println("Password must be at least 16 characters.")
+        os.Exit(0)
+	}
+
 
     var iData []byte
 	var oData []byte
 
-	cryptor := NewCryptor(pass1)
-	iData, err := ioutil.ReadFile(iFile)
+	cryptor, err := NewCryptor(pass1)
+    check(err)
+
+	iData, err = ioutil.ReadFile(iFile)
 	check(err)
 
 	switch action {
