4190adea9c9f001e9ea486088980430d01e0a589
Stephen Haywood <stephen@averagesecurityguy.info>
2018-01-03 00:10:19 -0500

    Use flag to parse args. Only ask for password once on decrypt.

--- crypt.go
+++ crypt.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"flag"
 	"fmt"
     "io/ioutil"
 	"os"
@@ -12,7 +13,15 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
+// Minimum password length.
+const passLen = 16
 
+// Static salt used to seed the Argon2 KDF. Generated with:
+// head -c 32 /dev/urandom | base64
+const salt = "AKatmtgdkMKq5SFYLt8tBlUxuwLccdCjFfFNi2b3o9A"
+
+
+// The Cryptor struct manages the encryption and decryption of data.
 type Cryptor struct {
 	Aead cipher.AEAD
 	Nonce []byte
@@ -26,14 +35,12 @@ func (c *Cryptor) Encrypt(plaintext []byte) []byte {
 	return c.Aead.Seal(nil, c.Nonce, plaintext, nil)
 }
 
+// Create a new Cryptor struct.
+// Both the encryption key and the nonce are derived from the passphrase.
 func NewCryptor(passphrase string) (*Cryptor, error) {
 	c := new(Cryptor)
 
-	// Static salt used to seed Argon2. Generated with:
-	// head -c 32 /dev/urandom | base64
-	salt := []byte("AKatmtgdkMKq5SFYLt8tBlUxuwLccdCjFfFNi2b3o9A")
-	kdf := argon2.Key([]byte(passphrase), salt, 4, 32*1024, 4, 44)
-
+	kdf := argon2.Key([]byte(passphrase), []byte(salt), 4, 32*1024, 4, 44)
 	c.Nonce = kdf[32:]
 
 	block, err := aes.NewCipher(kdf[:32])
@@ -51,7 +58,28 @@ func NewCryptor(passphrase string) (*Cryptor, error) {
 	return c, nil
 }
 
+// Get the encryption password from the user.
+// Ask the user to enter the password twice and verify the passwords match.
+// Also verify the password has at least passLen characters.
+func getEncPass() string {
+	pass1 := getPass("Enter password: ")
+	pass2 := getPass("Enter password again: ")
 
+	if pass1 != pass2 {
+		fmt.Println("Passwords do not match.")
+		os.Exit(0)
+	}
+
+	if len(pass1) < passLen {
+		fmt.Printf("Password must be at least %d characters.\n", passLen)
+		os.Exit(0)
+	}
+
+	return pass1
+}
+
+// Use x/crypto/ssh/terminal to get the user password without echoing to the
+// screen.
 func getPass(prompt string) string {
     fmt.Print(prompt)
     pass, err := terminal.ReadPassword(int(syscall.Stdin))
@@ -77,60 +105,43 @@ func check(e error) {
     }
 }
 
-
-func usage() {
-	fmt.Println("Usage: crypt command infile outfile")
-	fmt.Println("\tcommand must be either encrypt or decrypt.")
-	os.Exit(0)
-}
-
-
 func main() {
-	if len(os.Args) != 4 {
-		usage()
+	var action string
+	var inFile string
+	var outFile string
+	var password string
+	var inData []byte
+	var outData []byte
+
+	flag.StringVar(&action, "a", "encrypt", "Action to perform.")
+	flag.StringVar(&inFile, "i", "", "Input file.")
+	flag.StringVar(&outFile, "o", "", "Output file.")
+
+	flag.Parse()
+
+	switch action {
+	case "encrypt":
+		password = getEncPass()
+	case "decrypt":
+		password = getPass("Enter password: ")
+	default:
+		flag.Usage()
 	}
 
-	// Parse arguments
-	action := os.Args[1]
-	iFile := os.Args[2]
-	oFile := os.Args[3]
+	cryptor, err := NewCryptor(password)
+	check(err)
 
-	// Get Password
-	pass1 := getPass("Enter password: ")
-	pass2 := getPass("Enter password again: ")
-
-	// Compare passwords.
-	if pass1 != pass2 {
-		fmt.Println("Passwords do not match.")
-        os.Exit(0)
-	}
-
-    // Check password length
-    if len(pass1) < 16 {
-		fmt.Println("Password must be at least 16 characters.")
-        os.Exit(0)
-	}
-
-
-    var iData []byte
-	var oData []byte
-
-	cryptor, err := NewCryptor(pass1)
-    check(err)
-
-	iData, err = ioutil.ReadFile(iFile)
+	inData, err = ioutil.ReadFile(inFile)
 	check(err)
 
 	switch action {
 	case "encrypt":
-		oData = cryptor.Encrypt(iData)
-		save(oFile, oData)
+		outData = cryptor.Encrypt(inData)
+		save(outFile, outData)
 	case "decrypt":
-		oData, err := cryptor.Decrypt(iData)
+		outData, err := cryptor.Decrypt(inData)
 		check(err)
 
-		save(oFile, oData)
-	default:
-		usage()
+		save(outFile, outData)
 	}
 }
